4f1a2fe7f8feca8827ed2472e106a7d864155a83
Vanessa Lin <valin@berkeley.edu>
2018-09-04 22:50:22 -0700

    added test file for suggestions

--- emission/core/wrapper/suggestion_sys.py
+++ emission/core/wrapper/suggestion_sys.py
@@ -314,13 +314,96 @@ def calculate_yelp_server_suggestion(uuid):
     cleaned_sections = time_series.get_data_df("analysis/inferred_section", time_query = None)
     suggestion_trips = edb.get_suggestion_trips_db()
     #Go in reverse order because we check by most recent trip
-
+    counter = 40
     if len(cleaned_sections) == 0:
         return_obj['message'] = 'Suggestions will appear once you start taking trips!'
         return return_obj
+    for i in range(len(cleaned_sections) - 1, -1, -1):
+        counter -=1 
+        if counter < 0:
+            return return_obj
+        if cleaned_sections.iloc[i]["end_ts"] - cleaned_sections.iloc[i]["start_ts"] < 5 * 60:
+            continue
 
 
 
+
+# def calculate_single_suggestion(uuid):
+#     #Given a single UUID, create a suggestion for them
+#     return_obj = { 'message': "Good job walking and biking! No suggestion to show.",
+#     'savings': "0", 'start_lat' : '0.0', 'start_lon' : '0.0',
+#     'end_lat' : '0.0', 'end_lon' : '0.0', 'method' : 'bike'}
+#     all_users = pd.DataFrame(list(edb.get_uuid_db().find({}, {"uuid": 1, "_id": 0})))
+#     user_id = all_users.iloc[all_users[all_users.uuid == uuid].index.tolist()[0]].uuid
+#     time_series = esta.TimeSeries.get_time_series(user_id)
+#     cleaned_sections = time_series.get_data_df("analysis/inferred_section", time_query = None)
+#     suggestion_trips = edb.get_suggestion_trips_db()
+#     #Go in reverse order because we check by most recent trip
+#     counter = 40
+#     if len(cleaned_sections) == 0:
+#         return_obj['message'] = 'Suggestions will appear once you start taking trips!'
+#         return return_obj
+#     for i in range(len(cleaned_sections) - 1, -1, -1):
+#         counter -= 1
+#         if counter < 0:
+#             #Iterate 20 trips back
+#             return return_obj
+#         if cleaned_sections.iloc[i]["end_ts"] - cleaned_sections.iloc[i]["start_ts"] < 5 * 60:
+#             continue
+#         distance_in_miles = cleaned_sections.iloc[i]["distance"] * 0.000621371
+#         mode = cleaned_sections.iloc[i]["sensed_mode"]
+#         start_loc = cleaned_sections.iloc[i]["start_loc"]["coordinates"]
+#         start_lat = str(start_loc[0])
+#         start_lon = str(start_loc[1])
+#         trip_id = cleaned_sections.iloc[i]['trip_id']
+#         tripDict = suggestion_trips.find_one({'uuid': uuid})
+#         print(tripDict)
+#         end_loc = cleaned_sections.iloc[i]["end_loc"]["coordinates"]
+#         end_lat = str(end_loc[0])
+#         end_lon = str(end_loc[1])
+#         if mode == 5 and distance_in_miles >= 5 and distance_in_miles <= 15:
+#             logging.debug("15 >= distance >= 5 so I'm considering distance: " + str(distance_in_miles))
+#             #Suggest bus if it is car and distance between 5 and 15
+#             default_message = return_obj['message']
+#             try:
+#                 message = "Try public transportation from " + return_address_from_location(start_lon + "," + start_lat) + \
+#                 " to " + return_address_from_location(end_lon + "," + end_lat) + " (tap me to view)"
+#                 #savings per month, .465 kg co2/mile for car, 0.14323126 kg co2/mile for bus
+#                 savings = str(int(distance_in_miles * 30 * .465 - 0.14323126 * distance_in_miles * 30))
+#                 return {'message' : message, 'savings' : savings, 'start_lat' : start_lat,
+#                 'start_lon' : start_lon, 'end_lat' : end_lat, 'end_lon' : end_lon, 'method': 'public'}
+#                 insert_into_db(tripDict, trip_id, suggestion_trips, uuid)
+#                 break
+#             except ValueError as e:
+#                 return_obj['message'] = default_message
+#                 continue
+#         elif (mode == 5 or mode == 3 or mode == 4) and (distance_in_miles < 5 and distance_in_miles >= 1):
+#             logging.debug("5 > distance >= 1 so I'm considering distance: " + str(distance_in_miles))
+#             #Suggest bike if it is car/bus/train and distance between 5 and 1
+#             try:
+#                 message = "Try biking from " + return_address_from_location(start_lon + "," + start_lat) + \
+#                 " to " + return_address_from_location(end_lon + "," + end_lat) + " (tap me to view)"
+#                 savings = str(int(distance_in_miles * 30 * .465))  #savings per month, .465 kg co2/mile
+#                 insert_into_db(tripDict, trip_id, suggestion_trips, uuid)
+#                 return {'message' : message, 'savings' : savings, 'start_lat' : start_lat,
+#                 'start_lon' : start_lon, 'end_lat' : end_lat, 'end_lon' : end_lon, 'method': 'bike'}
+#                 break
+#             except:
+#                 continue
+#         elif (mode == 5 or mode == 3 or mode == 4) and (distance_in_miles < 1):
+#             logging.debug("1 > distance so I'm considering distance: " + str(distance_in_miles))
+#             #Suggest walking if it is car/bus/train and distance less than 1
+#             try:
+#                 message = "Try walking/biking from " + return_address_from_location(start_lon + "," + start_lat) + \
+#                 " to " + return_address_from_location(end_lon + "," + end_lat) + " (tap me to view)"
+#                 savings = str(int(distance_in_miles * 30 * .465)) #savings per month, .465 kg co2/mile
+#                 insert_into_db(tripDict, trip_id, suggestion_trips, uuid)
+#                 return {'message' : message, 'savings' : savings, 'start_lat' : start_lat,
+#                 'start_lon' : start_lon, 'end_lat' : end_lat, 'end_lon' : end_lon, 'method': 'walk'}
+#                 break
+#             except:
+#                 continue
+#     return return_obj
 
 
 
